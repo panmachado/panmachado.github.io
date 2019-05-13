cafa0ddc96685d2fb8c55aeba5cb57bb1897c436
Pedro Machado <pmachado@fnal.gov>
2019-05-13 08:55:03 -0500

    first pass
    see https://medium.com/@svinkle/publish-and-share-your-own-website-for-free-with-github-2eff049a1cb5

---
+++ Pedro_Machado_files/Pedro_Machado.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130425
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.37,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Pedro_Machado_files/Pedro_MachadoMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
